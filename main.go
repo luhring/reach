dbe36ee38b54a4c14fb30b1bdbaca73ab6bb3331
Dan Luhring <dan+github@luhrings.com>
2019-02-15 11:34:55 -0500

    Simplify main

--- main.go
+++ main.go
@@ -1,29 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/luhring/reach/cmd"
 )
 
 func main() {
-	instanceIdentificationInputs := cli.GetInstanceIdentificationInputsFromCli()
-	session, err := aws.CreateAwsSession()
-
-	if err != nil {
-		log.Fatalf("error: %v", err.Error())
-	}
-
-	ec2Client := ec2.New(session)
-	allInstances, err := aws.GetAllInstancesUsingEc2Client(ec2Client)
-
-	if err != nil {
-		log.Fatalf("error: %v", err.Error())
-	}
-
-	instancePair := cli.GetInstancePairFromIdentificationInputs(&instanceIdentificationInputs, allInstances)
-	resultMessage := instancePair.AnalyzeNetworkAccessAndGenerateResultMessage(ec2Client)
-
-	fmt.Print(resultMessage)
+	cmd.Execute()
 }
