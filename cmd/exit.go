ad9defe6bde386307ace60f2be9cdebed8168a18
Dan Luhring <dan+github@luhrings.com>
2019-10-14 08:43:57 -0400

    Add gap above assertion output message

--- cmd/exit.go
+++ cmd/exit.go
@@ -15,7 +15,7 @@ func exitWithError(err error) {
 func exitWithFailedAssertion(text string) {
 	failedMessage := ansi.Color("assertion failed:", "red+b")
 	secondaryMessage := ansi.Color(text, "red")
-	_, _ = fmt.Fprintf(os.Stderr, "%v %v\n", failedMessage, secondaryMessage)
+	_, _ = fmt.Fprintf(os.Stderr, "\n%v %v\n", failedMessage, secondaryMessage)
 
 	os.Exit(2)
 }
@@ -23,7 +23,7 @@ func exitWithFailedAssertion(text string) {
 func exitWithSuccessfulAssertion(text string) {
 	succeededMessage := ansi.Color("assertion succeeded:", "green+b")
 	secondaryMessage := ansi.Color(text, "green")
-	_, _ = fmt.Fprintf(os.Stderr, "%v %v\n", succeededMessage, secondaryMessage)
+	_, _ = fmt.Fprintf(os.Stderr, "\n%v %v\n", succeededMessage, secondaryMessage)
 
 	os.Exit(0)
 }
