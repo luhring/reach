7516a49f8fff84b44ff0e316f9df958e2e6ab5b4
Dan Luhring <dan+github@luhrings.com>
2019-10-24 21:00:38 -0400

    Allow OS specification in build script

--- build.sh
+++ build.sh
@@ -16,28 +16,31 @@ mkdir -p ./build
 pushd ./build
   for CURRENT_OS in "darwin" "linux" "windows"
   do
-    export GOOS=$CURRENT_OS
-
-    if [ $CURRENT_OS == "windows" ]
+    if [ -z $1 ] || [ $1 == $CURRENT_OS ]
     then
-      REACH_EXECUTABLE="reach.exe"
-    else
-      REACH_EXECUTABLE="reach"
-    fi
+      export GOOS=$CURRENT_OS
 
-    REACH_DIR_FOR_OS=$(printf "reach_%s_%s_amd64" $REACH_VERSION $CURRENT_OS)
-    mkdir -p ./$REACH_DIR_FOR_OS
+      if [ $CURRENT_OS == "windows" ]
+      then
+        REACH_EXECUTABLE="reach.exe"
+      else
+        REACH_EXECUTABLE="reach"
+      fi
 
-    go build -a -v -tags netgo -o "./$REACH_DIR_FOR_OS/$REACH_EXECUTABLE" ..
-    cp -nv ../LICENSE ../README.md "./$REACH_DIR_FOR_OS/"
+      REACH_DIR_FOR_OS=$(printf "reach_%s_%s_amd64" $REACH_VERSION $CURRENT_OS)
+      mkdir -p ./$REACH_DIR_FOR_OS
 
-    if [ $CURRENT_OS == "windows" ]
-    then
-      zip $REACH_DIR_FOR_OS.zip ./$REACH_DIR_FOR_OS/*
-      openssl dgst -sha256 ./$REACH_DIR_FOR_OS.zip >> ./checksums.txt
-    else
-      tar -cvzf $REACH_DIR_FOR_OS.tar.gz ./$REACH_DIR_FOR_OS/*
-      openssl dgst -sha256 ./$REACH_DIR_FOR_OS.tar.gz >> ./checksums.txt
+      go build -a -v -tags netgo -o "./$REACH_DIR_FOR_OS/$REACH_EXECUTABLE" ..
+      cp -nv ../LICENSE ../README.md "./$REACH_DIR_FOR_OS/"
+
+      if [ $CURRENT_OS == "windows" ]
+      then
+        zip $REACH_DIR_FOR_OS.zip ./$REACH_DIR_FOR_OS/*
+        openssl dgst -sha256 ./$REACH_DIR_FOR_OS.zip >> ./checksums.txt
+      else
+        tar -cvzf $REACH_DIR_FOR_OS.tar.gz ./$REACH_DIR_FOR_OS/*
+        openssl dgst -sha256 ./$REACH_DIR_FOR_OS.tar.gz >> ./checksums.txt
+      fi
     fi
 
   done
