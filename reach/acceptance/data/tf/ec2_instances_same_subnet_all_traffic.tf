7d25600ef594f0eb00753cabebe0c796617a23f6
Dan Luhring <dan+github@luhrings.com>
2019-11-15 21:39:52 -0500

    Extend AAT timeout

--- reach/acceptance/data/tf/ec2_instances_same_subnet_all_traffic.tf
+++ reach/acceptance/data/tf/ec2_instances_same_subnet_all_traffic.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "source" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.subnet_single.id}"
+  subnet_id = aws_subnet.subnet_1_of_1.id
 
-  security_groups = ["${aws_security_group.outbound_allow_all.id}"]
+  security_groups = [aws_security_group.outbound_allow_all.id]
 
   tags = {
     Name = "aat_source"
@@ -11,11 +11,11 @@ resource "aws_instance" "source" {
 }
 
 resource "aws_instance" "destination" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.subnet_single.id}"
+  subnet_id = aws_subnet.subnet_1_of_1.id
 
-  security_groups = ["${aws_security_group.inbound_allow_all.id}"]
+  security_groups = [aws_security_group.inbound_allow_all.id]
 
   tags = {
     Name = "aat_destination"
