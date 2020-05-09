c9c0dc577be48803bd70853b7d25f0a7c2a160e9
Dan Luhring <dan+github@luhrings.com>
2020-05-09 09:22:54 -0400

    Fix lint errors for the cache package

--- reach/cache/cache.go
+++ reach/cache/cache.go
@@ -4,11 +4,13 @@ import (
 	"sync"
 )
 
+// Cache is a simple in-memory implementation of the interface reach.Cache.
 type Cache struct {
 	l     sync.RWMutex
 	store map[string]interface{}
 }
 
+// New returns a new instance of Cache, ready to use.
 func New() Cache {
 	store := make(map[string]interface{})
 
@@ -18,6 +20,7 @@ func New() Cache {
 	}
 }
 
+// Get returns the object from the cache for the matching key, if one exists.
 func (c *Cache) Get(key string) interface{} {
 	c.l.RLock()
 	defer c.l.RUnlock()
@@ -25,6 +28,7 @@ func (c *Cache) Get(key string) interface{} {
 	return c.store[key]
 }
 
+// Put inserts or updates an object in the cache for the given key.
 func (c *Cache) Put(key string, value interface{}) {
 	c.l.Lock()
 	defer c.l.Unlock()
